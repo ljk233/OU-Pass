0f6490c12e268de73fcd9225ee3d86649fa18e00
Liam Kelly <liam.james.kelly@gmail.com>
2023-12-18 09:18:12 +0000

    Updated docstrings

--- src/data/_base/encoder.py
+++ src/data/_base/encoder.py
@@ -5,8 +5,8 @@ Purpose
 
 This module defines the base classes for encoders, which are used to convert
 data between different formats. It provides an abstract base class,
-`AbstractEncoder`, and a concrete implementation, `Encoder`, that serves as a
-foundation for creating encoders for specific data formats.
+`AbstractEncoder`, and a concrete implementation, `Encoder`, that serves
+as a foundation for creating encoders for specific data formats.
 
 Recommendation
 --------------
@@ -15,27 +15,12 @@ We strongly recommend that a shared universal in-memory data structure be
 agreed upon before development. This will ensure a consistent and standardised
 format for all data exchanged between different encoders.
 
-By adopting a shared data structure, encoders can focus solely on encoding
-and decoding data into and out of this universal format, simplifying their
-implementation and promoting interoperability.
-
 Benefits of a Unified Data Structure
 ------------------------------------
 
-Enhanced Interoperability:
-
-A shared data structure promotes interoperability between encoders, allowing
-them to seamlessly exchange data without data formatting discrepancies.
-
-Simplified Encoder Implementation:
-
-By focusing on encoding and decoding a standardized data format, encoder
-development becomes more straightforward and consistent.
-
-Improved Data Handling:
-
-A unified data structure facilitates consistent data handling across different
-components of the data processing pipeline.
+By adopting a shared data structure, encoders can focus solely on encoding
+and decoding data into and out of this universal format, simplifying their
+implementation and promoting interoperability.
 
 Classes
 -------
@@ -50,7 +35,7 @@ of the `__init__` and abstract methods are needed.
 `Encoder`:
 
 - A concrete implementation of the `AbstractEncoder` class.
-- Provides the default implementation for the `__init__` method.
+- Provides a default implementation for the `__init__` method.
 - If a concrete class inherits from this class, then only specific only
 implementations are needed, but the `__init__` method can also be extended
 if wanted.
