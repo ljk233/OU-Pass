0c64c8e6126b0694e3bb94dd51f0c526fcf5a3d2
Liam Kelly <liam.james.kelly@gmail.com>
2023-12-18 14:23:53 +0000

    Updated the API

--- src/api.py
+++ src/api.py
@@ -2,4 +2,10 @@
 """
 
 from src.load_parameters import load_parameters
-from src.data.handling import create_data_handler
+from src.data.encoders import create_encoder
+from src.data.validators import create_data_validator
+from src.data.cleaners import create_cleaner
+from src.features.builders import create_feature_builder
+from src.model.split_data import train_test_split
+from src.model.trainers import create_model_trainer
+from src.model.testers import create_model_tester
